74a0f9ba60a002e9f1c3ba1928ff861c718da300
jys77 <jiang-yunsheng@hotmail.com>
2020-05-26 22:57:45 -0400

    feat: tasks main content

--- src/components/IndividualProject.js
+++ src/components/IndividualProject.js
@@ -22,25 +22,37 @@ export const IndividualProject = ({ project }) => {
 
   return (
     <>
-      <span className="sidebar__dot">●</span>
+      <span className="sidebar__dot">•</span>
       <span className="sidebar__project-name">{project.name}</span>
       <span
         className="sidebar__project-delete"
         data-testid="delete-project"
         onClick={() => setShowConfirm(!showConfirm)}
+        onKeyDown={() => setShowConfirm(!showConfirm)}
+        tabIndex={0}
+        role="button"
+        aria-label="Confirm deletion of project"
       >
         <FaTrashAlt />
         {showConfirm && (
           <div className="project-delete-modal">
             <div className="project-delete-modal__inner">
-              <p>Are you show you want to delete this project?</p>
+              <p>Are you sure you want to delete this project?</p>
               <button
                 type="button"
                 onClick={() => deleteProject(project.docId)}
               >
                 Delete
-                <span onClick={() => setShowConfirm(!showConfirm)}>Cancel</span>
               </button>
+              <span
+                onClick={() => setShowConfirm(!showConfirm)}
+                onKeyDown={() => setShowConfirm(!showConfirm)}
+                tabIndex={0}
+                role="button"
+                aria-label="Cancel adding project, do not delete"
+              >
+                Cancel
+              </span>
             </div>
           </div>
         )}
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Checkbox } from "./Checkbox";
 import { useTasks } from "../hooks";
+import { useSelectedProjectValue, useProjectsValue } from "../context";
+import { collatedTasks } from "../constants";
+import { getTitle, getCollatedTitle, collatedTasksExist } from "../helpers";
 export const Tasks = () => {
-  const { tasks } = useTasks("1");
-  console.log(tasks);
+  const { selectedProject } = useSelectedProjectValue();
+  const { projects } = useProjectsValue();
+  const { tasks } = useTasks(selectedProject);
 
+  console.log(selectedProject);
   let projectName = "";
+
+  if (projects && selectedProject && !collatedTasksExist(selectedProject)) {
+    projectName = getTitle(projects, selectedProject).name;
+    console.log("project 1", projectName);
+  }
+
+  if (collatedTasksExist(selectedProject) && selectedProject) {
+    projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+    console.log("project 2", projectName);
+  }
+
+  useEffect(() => {
+    document.title = `${projectName}: Todoist`;
+  }, []);
+
+  console.log("tasks", tasks);
 
   return (
     <div className="tasks" data-testid="tasks">
