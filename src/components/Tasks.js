34b9fe4ff8af8fce42dbbe4c53fa9af57c4a7c3d
jys77 <jiang-yunsheng@hotmail.com>
2020-05-28 00:01:15 -0400

    feat: add task component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,6 +4,7 @@ import { useTasks } from "../hooks";
 import { useSelectedProjectValue, useProjectsValue } from "../context";
 import { collatedTasks } from "../constants";
 import { getTitle, getCollatedTitle, collatedTasksExist } from "../helpers";
+import { AddTask } from "./AddTask";
 export const Tasks = () => {
   const { selectedProject } = useSelectedProjectValue();
   const { projects } = useProjectsValue();
@@ -39,6 +40,7 @@ export const Tasks = () => {
           </li>
         ))}
       </ul>
+      <AddTask />
     </div>
   );
 };
